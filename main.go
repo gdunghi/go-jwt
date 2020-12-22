02908a2161730cece9c195e8de47667d79b34f06
tomm <gdunghi@gmail.com>
2020-12-23 00:21:54 +0700

    init

--- main.go
+++ main.go
@@ -1,22 +1,65 @@
 package main
 
 import (
-    "net/http"
-    "github.com/labstack/echo"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+	"time"
 )
 
+type handler struct{}
+
+type auth struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
-    e := echo.New()
+	e := echo.New()
+	var IsLoggedIn = middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey: []byte("secret"),
+	})
 
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 
-    h := &handler{}
-    e.POST("/login", h.login)
-    e.GET("/is-loggedin", h.private, IsLoggedIn)
-    e.GET("/is-admin", h.private, IsLoggedIn, isAdmin)
-    e.POST("/refresh", h.token)
+	h := &handler{}
+	e.POST("/login", h.login)
+	e.GET("/private", h.private, IsLoggedIn)
 
-    e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func (h *handler) login(c echo.Context) error {
+	a := auth{}
+	c.Bind(&a)
+
+	if a.Username == "odds" && a.Password == "password" {
+		// Create token
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "odds"
+		claims["exp"] = time.Now().Add(time.Minute * 5).Unix()
+
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+	}
+	return echo.ErrUnauthorized
+}
+
+// Most of the code is taken from the echo guide
+// https://echo.labstack.com/cookbook/jwt
+func (h *handler) private(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.String(http.StatusOK, "Welcome "+name+"!")
 }
